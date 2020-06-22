0c5af2367f08dc153e63fbd1a303083b527f4f8a
Lio320 <saracenileonardo01@gmail.com>
2020-06-22 12:22:42 +0200

    movements

--- main.js
+++ main.js
@@ -3,96 +3,219 @@ import {OBJLoader2} from './threejs/examples/jsm/loaders/OBJLoader2.js';
 import {GLTFLoader} from './threejs/examples/jsm/loaders/GLTFLoader.js';
 import {OrbitControls} from './threejs/examples/jsm/controls/OrbitControls.js';
 import { Sky } from './threejs/examples/jsm/objects/Sky.js';
-import {F}
+import { PointerLockControls } from './threejs/examples/jsm/controls/PointerLockControls.js';
 
 var camera, scene, renderer;
 var geometry, material;
 var sky, sunSphere;
+
+// Controls variables
+var controls;
+var raycaster;
+var objects = [];
+
+var moveForward = false;
+var moveBackward = false;
+var moveLeft = false;
+var moveRight = false;
+var canJump = false;
+
+var prevTime = performance.now();
+var velocity = new THREE.Vector3();
+var direction = new THREE.Vector3();
 
 init();
 animate();
 
 function init() {
 	// renderer
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
     
 
-    // scene
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0x0000ff );
+  // scene
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color( 0x0000ff );
     
   // camera, position it and point 
   camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
   camera.position.y = 1;
   camera.position.y = 1;
   camera.position.z = 100;
-  camera.lookAt(0, 0, 0)	
+  camera.lookAt(0, 0, 0);	
   
   // Control camera
-  var camControls = new THREE.FirstPersonControls(camera);
-  camControls.lookSpeed = 0.4;
-  camControls.movementSpeed = 20;
-  camControls.noFly = true;
-  camControls.lookVertical = true;
-  camControls.constrainVertical = true;
-  camControls.verticalMin = 1.0;
-  camControls.verticalMax = 2.0;
-  camControls.lon = -150;
-  camControls.lat = 120;
-  
+  controls = new PointerLockControls(camera, document.body);
+  controls.isLocked = true;
+  scene.add( controls.getObject() );
+  var onKeyDown = function ( event ) {
+
+    switch ( event.keyCode ) {
+
+      case 38: // up
+      case 87: // w
+        moveForward = true;
+        break;
+
+      case 37: // left
+      case 65: // a
+        moveLeft = true;
+        break;
+
+      case 40: // down
+      case 83: // s
+        moveBackward = true;
+        break;
+
+      case 39: // right
+      case 68: // d
+        moveRight = true;
+        break;
+
+      case 32: // space
+        if ( canJump === true ) velocity.y += 350;
+        canJump = false;
+        break;
+
+    }
+
+  };
+
+  var onKeyUp = function ( event ) {
+
+    switch ( event.keyCode ) {
+
+      case 38: // up
+      case 87: // w
+        moveForward = false;
+        break;
+
+      case 37: // left
+      case 65: // a
+        moveLeft = false;
+        break;
+
+      case 40: // down
+      case 83: // s
+        moveBackward = false;
+        break;
+
+      case 39: // right
+      case 68: // d
+        moveRight = false;
+        break;
+
+    }
+
+  };
+
+  document.addEventListener( 'keydown', onKeyDown, false );
+  document.addEventListener( 'keyup', onKeyUp, false );
+
+  raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
   /*
 	// control camera
 	const controls = new OrbitControls(camera, renderer.domElement);
-  	controls.target.set(0, 0, 0);
-  	controls.update();
+  controls.target.set(0, 0, 0);
+  controls.update();
   */
   
-    // ground
-    var loader = new THREE.TextureLoader();
+  
+  // ground
+  var loader = new THREE.TextureLoader();
 
-    var groundTexture = loader.load( 'textures/ground.png' );
-    groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
-    groundTexture.repeat.set( 2000, 2000 );
-    groundTexture.anisotropy = 16;
-    groundTexture.encoding = THREE.sRGBEncoding;
+  var groundTexture = loader.load( 'textures/ground.png' );
+  groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+  groundTexture.repeat.set( 2000, 2000 );
+  groundTexture.anisotropy = 16;
+  groundTexture.encoding = THREE.sRGBEncoding;
 
-    var groundMaterial = new THREE.MeshPhongMaterial( { map: groundTexture, side: THREE.DoubleSide } );
+  var groundMaterial = new THREE.MeshPhongMaterial( { map: groundTexture, side: THREE.DoubleSide } );
 
-    var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 20000, 20000 ), groundMaterial );
-    mesh.rotation.x = - Math.PI / 2;
-    mesh.receiveShadow = true;
-    scene.add( mesh );
+  var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 20000, 20000 ), groundMaterial );
+  mesh.rotation.x = - Math.PI / 2;
+  mesh.receiveShadow = true;
+  scene.add( mesh );
     
 	// models
-    {    
-      const gltfLoader = new GLTFLoader();
-      const url = './threejs/Models/cartoon_lowpoly_small_city_free_pack/scene.gltf';
-      gltfLoader.load(url, (gltf) => {
-        const root = gltf.scene;
-        root.scale.set( 0.1, 0.1, 0.1);
-        root.position.z = 0;
-        root.position.y = 12;
-        scene.add(root);
-      });
-    }
-    
-    initSky();
+  {    
+    const gltfLoader = new GLTFLoader();
+    const url = './threejs/Models/cartoon_lowpoly_small_city_free_pack/scene.gltf';
+    gltfLoader.load(url, (gltf) => {
+      const root = gltf.scene;
+      root.scale.set( 0.1, 0.1, 0.1);
+      root.position.z = 0;
+      root.position.y = 12;
+      scene.add(root);
+      objects.push(root);
+    });
+  }
+  
+  initSky();
 
-    window.addEventListener( 'resize', onWindowResize, false );
+  window.addEventListener( 'resize', onWindowResize, false );
 }
 
 function animate() {
-	requestAnimationFrame( animate );
+  requestAnimationFrame( animate );
+  
+  if ( controls.isLocked === true ) {
+
+    raycaster.ray.origin.copy( controls.getObject().position );
+    raycaster.ray.origin.y -= 10;
+
+    var intersections = raycaster.intersectObjects( objects );
+
+    var onObject = intersections.length > 0;
+
+    var time = performance.now();
+    var delta = ( time - prevTime ) / 1000;
+
+    velocity.x -= velocity.x * 10.0 * delta;
+    velocity.z -= velocity.z * 10.0 * delta;
+
+    velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+    direction.z = Number( moveForward ) - Number( moveBackward );
+    direction.x = Number( moveRight ) - Number( moveLeft );
+    direction.normalize(); // this ensures consistent movements in all directions
+
+    if ( moveForward || moveBackward ) velocity.z -= direction.z * 400.0 * delta;
+    if ( moveLeft || moveRight ) velocity.x -= direction.x * 400.0 * delta;
+
+    if ( onObject === true ) {
+
+      velocity.y = Math.max( 0, velocity.y );
+      canJump = true;
+
+    }
+
+    controls.moveRight( - velocity.x * delta );
+    controls.moveForward( - velocity.z * delta );
+
+    controls.getObject().position.y += ( velocity.y * delta ); // new behavior
+
+    if ( controls.getObject().position.y < 10 ) {
+
+      velocity.y = 0;
+      controls.getObject().position.y = 10;
+
+      canJump = true;
+
+    }
+
+    prevTime = time;
+
+  }
 
 	renderer.render( scene, camera );
 }
 
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
 	renderer.setSize( window.innerWidth, window.innerHeight );
 }
