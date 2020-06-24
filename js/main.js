aa05fa4251e121f65bf95b0b8db64208d785ef0b
Ionut Motoi <32934655+IonutMotoi@users.noreply.github.com>
2020-06-24 18:20:38 +0200

    Babylon with npm

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import * as BABYLON from '@babylonjs/core/Legacy/legacy';
+
 // Canvas
 var canvas = document.getElementById('renderCanvas');
 // 3D Engine
@@ -9,21 +11,9 @@ var createScene = function() {
     scene.gravity = new BABYLON.Vector3(0, -0.98, 0);
 
     // Camera
-    var camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 2,-10), scene);
+    var camera = new BABYLON.FollowCamera('camera', new BABYLON.Vector3(0, 2,-10), scene);
     camera.setTarget(BABYLON.Vector3.Zero());
     camera.attachControl(canvas, true);
-    camera.checkCollisions = true;
-    camera.applyGravity = true; 
-    camera._needMoveForGravity = true; // Magic line 17
-    camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
-
-    // Camera controls
-    camera.inertia = 0;
-    camera.keysUp = [87]; // W
-    camera.keysDown = [83]; // S
-    camera.keysLeft = [65]; // A
-    camera.keysRight = [68]; // D
-    camera.speed = 5;
 
     // Light
     var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
