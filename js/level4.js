dc303b51363b6a9da0d47c1111064058be00abef
Lio320 <saracenileonardo01@gmail.com>
2020-07-23 12:04:26 +0200

    update the sun! tututututurututut

--- js/level4.js
+++ js/level4.js
@@ -29,7 +29,6 @@ var snowLA2;
 
 var coin;
 
-var sun;
 
 var tutorial1 = false;
 var tutorial2 = false;
@@ -40,6 +39,7 @@ var tutorial6 = false;
 
 var snowAnim = true;
 
+var nextLevel = "../level1.html"
 
 var checkpoint = new BABYLON.Vector3(0, 10, 0);
 
@@ -504,11 +504,9 @@ var createScene = function() {
 
     //Sun
     BABYLON.SceneLoader.ImportMesh("", "../models/SummerModels/", "sun.gltf", scene, function(newMeshes) {
-        var sun = newMeshes[0];
+        sun = newMeshes[0];
         sun.scaling = new BABYLON.Vector3(5,5,5);
-        sun.parent = camera.x;
-        sun.position = new BABYLON.Vector3(10,30,100);
-
+        sun.position = new BABYLON.Vector3(10, 20, 75);
     });
 
     // Palm 1
@@ -549,6 +547,22 @@ var createScene = function() {
         ball.position = new BABYLON.Vector3(-2,1,9);
         ball.scaling = new BABYLON.Vector3(3,3,3);
         ball.rotate(new BABYLON.Vector3(0,1,0), 0);
+    });
+
+    // Beach ball
+    BABYLON.SceneLoader.ImportMesh("", "../models/SummerModels/", "ball.gltf", scene, function(newMeshes) {
+        var ball = newMeshes[0];
+        ball.position = new BABYLON.Vector3(-2,1,9);
+        ball.scaling = new BABYLON.Vector3(3,3,3);
+        ball.rotate(new BABYLON.Vector3(0,1,0), 0);
+    });
+
+    // Umbrella sdraio table
+    BABYLON.SceneLoader.ImportMesh("", "../models/SummerModels/", "umbrellaSdraioTable.gltf", scene, function(newMeshes) {
+        var umbrellaSdraioTable = newMeshes[0];
+        umbrellaSdraioTable.position = new BABYLON.Vector3(195,0,8);
+        umbrellaSdraioTable.scaling = new BABYLON.Vector3(2.8,2.8,2.8);
+        umbrellaSdraioTable.rotate(new BABYLON.Vector3(0,1,0), 0);
     });
 
 
@@ -741,16 +755,18 @@ var createScene = function() {
 
     scene.executeWhenReady( function() {
         engine.hideLoadingUI();
+        scene.registerAfterRender(function () {
+            sun.position.x = player.mesh.position.x;
+        });
     });
-
-    scene.registerAfterRender(function () {
-        //sun.position.x = 0.4;
-    });
-
+    
+    
     return scene;
 }
 
+
 var scene = createScene();
+
 
 function coinON(){
     CoinDisappear(coin1);
